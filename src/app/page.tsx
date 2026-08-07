ef0d5da943079fce05c54358a2a5db78c0955ab6
raisalomi595 <raisalomi595@gmail.com>
2026-08-07 21:15:20 +0545

    Feat: add category chips and navigation links for history and watch later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import TopNav from '@/components/layout/TopNav'
 import Sidebar from '@/components/layout/Sidebar'
 import MobileNav from '@/components/layout/MobileNav'
+import CategoryChips from '@/components/home/CategoryChips'
 import VideoGrid from '@/components/video/VideoGrid'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="flex">
         <Sidebar />
         <main className="min-w-0 flex-1 px-4 py-6 sm:px-6">
+          <CategoryChips />
           <VideoGrid />
         </main>
       </div>
